9434837dc824090ffafe32dcf14783ee45eecc54
Oswell <oswell.ndhlovu@umuzi.org>
2025-02-20 23:59:24 +0200

    add some additions to the home page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Contact from "./components/contact/Contact";
 const App = () => {
   return (
     <div className="container">
-      <section id ="#home"> <Hero /></section>
+      <section id ="#home"> <starsCanvas/> <Hero /></section>
       <section id ="#services"><Services /></section>
       <section id ="#portfolio"><Portfolio /></section>
       <section id ="#contact"><Contact /></section>
--- src/components/hero/Speech.jsx
+++ src/components/hero/Speech.jsx
@@ -1,8 +1,28 @@
+import { TypeAnimation } from 'react-type-animation';
+
 const Speech = () => {
     return (
         <div className="bubbleContainer">
             <div className="bubble">
-            text
+            <TypeAnimation
+      sequence={[
+        1000,
+        "Same substring at the start will only be typed out once, initially",
+        1000,
+        'I code in Python',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        'I code in JavaScript',
+        1000,
+        'I code in HTML',
+        1000,
+        'I code in CSS',
+        1000
+      ]}
+      wrapper="span"
+      speed={40}
+      deletionSpeed={60}
+      repeat={Infinity}
+    />
             </div>
             <img src="/man.png" alt=""/>
         </div>
